9afe4c84603197cb00331dd300eda54dbd3d9ce8
Omar Sabry <omarahmedsabry05@gmail.com>
2025-10-14 12:34:02 +0300

    Update Interceptor in @bts-soft/common

--- packages/common/src/interceptors/main.interceptor.ts
+++ packages/common/src/interceptors/main.interceptor.ts
@@ -1,10 +1,13 @@
-import { INestApplication } from '@nestjs/common';
-import { GeneralResponseInterceptor } from './generalResponse.interceptor';
-import { SqlInjectionInterceptor } from './sqlInjection.interceptor';
+import { GeneralResponseInterceptor } from "./generalResponse.interceptor";
+import { SqlInjectionInterceptor } from "./sqlInjection.interceptor";
+import { ClassSerializerInterceptor, INestApplication } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 
 export const setupInterceptors = (app: INestApplication): void => {
   app.useGlobalInterceptors(
+    new ClassSerializerInterceptor(app.get(Reflector)),
     new SqlInjectionInterceptor(),
     new GeneralResponseInterceptor(),
+    new ClassSerializerInterceptor(app.get(Reflector))
   );
 };
