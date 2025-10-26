b28866eddb1ecbcf31b6bd657197c139bec849e9
Omar Sabry <omarahmedsabry05@gmail.com>
2025-10-26 14:37:48 +0300

    fix error in @bts-soft/upload in config

--- packages/upload/src/upload/upload.module.ts
+++ packages/upload/src/upload/upload.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UploadService } from './upload.service';
+import { Module } from "@nestjs/common";
+import { UploadService } from "./upload.service";
+import { ConfigModule } from "@nestjs/config";
 
 /**
  * NestJS module providing the UploadService for Cloudinary uploads.
  * It also includes the REST API Controller for standard HTTP file uploads.
  */
 @Module({
+  imports: [ConfigModule],
   providers: [UploadService],
   exports: [UploadService],
 })
